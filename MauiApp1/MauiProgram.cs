7841a17a4b570be333f8b1960e9adf1d3ad9d8d3
Brendan Bryan <bbryan1294@gmail.com>
2023-10-03 16:17:13 -0500

    Add project files.

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using MauiApp1.ViewModel;
+using Microsoft.Extensions.Logging;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddSingleton<MainViewModel>();
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
---
+++ MauiApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace MauiApp1.ViewModel;
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [RelayCommand]
+    void Add()
+    {
+        if (string.IsNullOrWhiteSpace(Text))
+            return;
+
+        Items.Add(Text);
+        // add our item
+        Text = string.Empty;
+    }
+
+    [RelayCommand]
+    void Delete(string s)
+    {
+        if(Items.Contains(s))
+            Items.Remove(s);
+    }
+}
+
